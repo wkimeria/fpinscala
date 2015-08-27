6f825c3049747676f0fbb8b50c3286fdf8d9740e
Will Kimeria <william.kimeria@lookout.com>
2015-08-27 09:37:09 -0700

    Code reformatting

--- exercises/src/main/scala/fpinscala/datastructures/List.scala
+++ exercises/src/main/scala/fpinscala/datastructures/List.scala
@@ -1,29 +1,35 @@
 package fpinscala.datastructures
 
-sealed trait List[+A] // `List` data type, parameterized on a type, `A`
-case object Nil extends List[Nothing] // A `List` data constructor representing the empty list
+sealed trait List[+A]
+
+// `List` data type, parameterized on a type, `A`
+case object Nil extends List[Nothing]
+
+// A `List` data constructor representing the empty list
 /* Another data constructor, representing nonempty lists. Note that `tail` is another `List[A]`,
 which may be `Nil` or another `Cons`.
  */
 case class Cons[+A](head: A, tail: List[A]) extends List[A]
 
-object List { // `List` companion object. Contains functions for creating and working with lists.
-  def sum(ints: List[Int]): Int = ints match { // A function that uses pattern matching to add up a list of integers
+object List {
+  // `List` companion object. Contains functions for creating and working with lists.
+  def sum(ints: List[Int]): Int = ints match {
+    // A function that uses pattern matching to add up a list of integers
     case Nil => 0 // The sum of the empty list is 0.
-    case Cons(x,xs) => x + sum(xs) // The sum of a list starting with `x` is `x` plus the sum of the rest of the list.
+    case Cons(x, xs) => x + sum(xs) // The sum of a list starting with `x` is `x` plus the sum of the rest of the list.
   }
 
   def product(ds: List[Double]): Double = ds match {
     case Nil => 1.0
     case Cons(0.0, _) => 0.0
-    case Cons(x,xs) => x * product(xs)
+    case Cons(x, xs) => x * product(xs)
   }
 
   def apply[A](as: A*): List[A] = // Variadic function syntax
     if (as.isEmpty) Nil
     else Cons(as.head, apply(as.tail: _*))
 
-  val x = List(1,2,3,4,5) match {
+  val x = List(1, 2, 3, 4, 5) match {
     case Cons(x, Cons(2, Cons(4, _))) => x
     case Nil => 42
     case Cons(x, Cons(y, Cons(3, Cons(4, _)))) => x + y
@@ -34,17 +40,17 @@ object List { // `List` companion object. Contains functions for creating and wo
   def append[A](a1: List[A], a2: List[A]): List[A] =
     a1 match {
       case Nil => a2
-      case Cons(h,t) => Cons(h, append(t, a2))
+      case Cons(h, t) => Cons(h, append(t, a2))
     }
 
-  def foldRight[A,B](as: List[A], z: B)(f: (A, B) => B): B = // Utility functions
+  def foldRight[A, B](as: List[A], z: B)(f: (A, B) => B): B = // Utility functions
     as match {
       case Nil => z
       case Cons(x, xs) => f(x, foldRight(xs, z)(f))
     }
 
   def sum2(ns: List[Int]) =
-    foldRight(ns, 0)((x,y) => x + y)
+    foldRight(ns, 0)((x, y) => x + y)
 
   def product2(ns: List[Double]) =
     foldRight(ns, 1.0)(_ * _) // `_ * _` is more concise notation for `(x,y) => x * y`; see sidebar
@@ -57,7 +63,7 @@ object List { // `List` companion object. Contains functions for creating and wo
   What are different choices you could make in your implementation if the List is Nil?
    */
   def tail[A](l: List[A]): List[A] = l match {
-    case Cons(_,x) => x
+    case Cons(_, x) => x
     case Nil => Nil
   }
 
@@ -66,8 +72,8 @@ object List { // `List` companion object. Contains functions for creating and wo
 
   Using the same idea, implement the function setHead for replacing the first element of a List with a different value.
    */
-  def setHead[A](l: List[A], h: A): List[A] =  l match {
-    case Cons(_,x) => Cons(h, x)
+  def setHead[A](l: List[A], h: A): List[A] = l match {
+    case Cons(_, x) => Cons(h, x)
     case Nil => Nil
   }
 
@@ -79,13 +85,13 @@ object List { // `List` companion object. Contains functions for creating and wo
    */
   @annotation.tailrec
   def drop[A](l: List[A], n: Int): List[A] =
-  if (n == 0) l
-  else {
-    l match {
-      case Nil => Nil
-      case Cons(_, t) => drop(t, n - 1)
+    if (n == 0) l
+    else {
+      l match {
+        case Nil => Nil
+        case Cons(_, t) => drop(t, n - 1)
+      }
     }
-  }
 
   /*
   Exercise 3.5
@@ -95,8 +101,8 @@ object List { // `List` companion object. Contains functions for creating and wo
     l match {
       case Nil => Nil
       case Cons(h, t) => {
-        if(f(h) == true) dropWhile(t,f)
-        else Cons(h,dropWhile(t,f))
+        if (f(h) == true) dropWhile(t, f)
+        else Cons(h, dropWhile(t, f))
       }
     }
 
@@ -109,8 +115,8 @@ object List { // `List` companion object. Contains functions for creating and wo
   def init[A](l: List[A]): List[A] = {
     def loop(a: List[A]): List[A] = {
       a match {
-        case Cons(h,Nil) => Nil
-        case Cons(h,t) => Cons(h, loop(t))
+        case Cons(h, Nil) => Nil
+        case Cons(h, t) => Cons(h, loop(t))
         case Nil => Nil
       }
     }
@@ -119,34 +125,36 @@ object List { // `List` companion object. Contains functions for creating and wo
 
   def length[A](l: List[A]): Int = sys.error("todo")
 
-  def foldLeft[A,B](l: List[A], z: B)(f: (B, A) => B): B = sys.error("todo")
+  def foldLeft[A, B](l: List[A], z: B)(f: (B, A) => B): B = sys.error("todo")
 
-  def map[A,B](l: List[A])(f: A => B): List[B] = sys.error("todo")
+  def map[A, B](l: List[A])(f: A => B): List[B] = sys.error("todo")
 
 }
 
-object TestList{
+object TestList {
+
   import List._
+
   def main(args: Array[String]): Unit = {
     //Test tail
-    assert(tail(List(1,2,3,4,5)) == List(2,3,4,5))
+    assert(tail(List(1, 2, 3, 4, 5)) == List(2, 3, 4, 5))
     assert(tail(List(1)) == Nil)
     assert(tail(Nil) == Nil)
 
     //Test Head
-    assert(setHead(List(1,2,3,4,5), 99) == List(99, 2,3,4,5))
+    assert(setHead(List(1, 2, 3, 4, 5), 99) == List(99, 2, 3, 4, 5))
 
     //Test drop
-    assert(drop(List(1,2,3,4,5),1) == List(2,3,4,5))
-    assert(drop(List(1,2,3,4,5),2) == List(3,4,5))
-    assert(drop(List(1,2,3,4,5),3) == List(4,5))
+    assert(drop(List(1, 2, 3, 4, 5), 1) == List(2, 3, 4, 5))
+    assert(drop(List(1, 2, 3, 4, 5), 2) == List(3, 4, 5))
+    assert(drop(List(1, 2, 3, 4, 5), 3) == List(4, 5))
 
     //Test dropWhile
-    assert(dropWhile(List(1,2,3,4,5,6),((x: Int)=> x % 2 == 0)) == List(1,3,5))
+    assert(dropWhile(List(1, 2, 3, 4, 5, 6), ((x: Int) => x % 2 == 0)) == List(1, 3, 5))
 
     //Test
     assert(init(List(1)) == Nil)
-    assert(init(List(1,2,3,4,5)) == List(1, 2,3,4))
+    assert(init(List(1, 2, 3, 4, 5)) == List(1, 2, 3, 4))
 
 
   }
