5f3aa0c2a353cd6e757d0d9407a27e364c31e4d7
Will Kimeria <william.kimeria@lookout.com>
2015-09-04 09:26:55 -0700

    Exercise 3:25

--- exercises/src/main/scala/fpinscala/datastructures/Tree.scala
+++ exercises/src/main/scala/fpinscala/datastructures/Tree.scala
@@ -7,7 +7,28 @@ case class Branch[A](left: Tree[A], right: Tree[A]) extends Tree[A]
 
 object Tree {
 
+  /*
+  Exercise 3:25
+  Write a function size that counts the number of nodes (leaves and branches) in a tree.
+  */
+  def count[A](tree: Tree[A]): Int = {
+    def loop(t: Tree[A], c: Int): Int = t match {
+      case Leaf(x) => c + 1
+      case Branch(l,r) => 1 + loop(l,c) + loop(r, c)
+    }
+    loop(tree, 0)
+  }
+}
 
+object TestTree {
 
+  import Tree._
 
+  def main(args: Array[String]): Unit = {
+
+    //Test count
+    assert(count(Branch(Leaf(1),Leaf(2))) == 3)
+    assert(count(Branch(Leaf(9),Branch(Leaf(1),Leaf(2)))) == 5)
+
+  }
 }
